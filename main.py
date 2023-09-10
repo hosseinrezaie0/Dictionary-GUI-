859ba2bf791d5a4bb41f9d590a9e0efbdeb64202
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-09-10 17:22:59 +0330

    UI section - title added

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import pyperclip
 
+
+
+#-----------------------------------UI-----------------------------------#
+root = ttk.Window(themename="superhero")
+root.title("Dictionary")
+root.geometry("500x500")
+# root.config(padx=20,pady=20)
+
+label = ttk.Label(text="German to English",font=("Arial",25))
+label.grid(row=0,column=1,columnspan=6)
+
+
+
+
+
+root.mainloop()
+
+
+
