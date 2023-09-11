9dd8ad94debed0e0f9f9093f5f4c50ba8ba680f0
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-09-11 21:05:05 +0330

    add english label and english output

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pyperclip
 #-----------------------------------UI-----------------------------------#
 root = ttk.Window(themename="superhero")
 root.title("Dictionary")
-root.geometry("700x700")
+root.geometry("870x700")
 root.resizable(0,0)
 # root.config(padx=50)
 
@@ -24,28 +24,17 @@ canavs.place(x=0,y=0)
 
 du_label = ttk.Label(text="German",width=50)
 du_label.place(x=50,y=200)
-
 input_text = ttk.Text(wrap=ttk.WORD,height=10,width=25)
 input_text.place(x=50,y=250)
-# du_text = ttk.ScrolledText(root, wrap=ttk.WORD,
-#                                       width=20, height=8,
-#                                       font=("Times New Roman", 15))
-  
-# du_text.grid(row=2,column=0,sticky="w")
 
-
-# en_label = ttk.Label(text="English",width=50)
-# en_label.grid(row=1,column=2)
-
-# en_text = ttk.ScrolledText(root, wrap=ttk.WORD,
-#                                       width=20, height=8,
-#                                       font=("Times New Roman", 15))
-  
-# en_text.grid(row=2,column=2,sticky="w")
+en_label = ttk.Label(text="English")
+en_label.place(x=500,y=200)
+output_text = ttk.Text(wrap=WORD,height=10,width=25)
+output_text.place(x=500,y=250)
 
 
 
-# du_text.focus()
+
 root.mainloop()
 
 
