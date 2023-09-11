819ec612b300f65d7be97a7b4517a648fe78be13
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-09-11 19:32:23 +0330

    logo added to screen

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ import pyperclip
 #-----------------------------------UI-----------------------------------#
 root = ttk.Window(themename="superhero")
 root.title("Dictionary")
-root.geometry("500x500")
+root.geometry("700x700")
 # root.config(padx=20,pady=20)
 
-label = ttk.Label(text="German to English",font=("Arial",25))
-label.grid(row=0,column=1,columnspan=6)
+# label = ttk.Label(text="German to English",font=("Arial",25))
+# label.grid(row=0,column=0,columnspan=2)
+
+canavs = ttk.Canvas(width=256,height=256)
+file = ttk.PhotoImage(file="logo.png")
+img = canavs.create_image(128,128,image=file)
+canavs.grid(row=0,column=0)
 
 
 
